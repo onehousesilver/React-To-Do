5ff527c75f41ffe35de1c7974cf83d7d13e3f961
onehousesilver <onehousesilver@naver.com>
2022-03-15 00:57:06 +0900

    Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -34,17 +34,45 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("firstname")} placeholder="firstname" />
-        <input {...register("lastname")} placeholder="lastname" />
-        <input {...register("username")} placeholder="username" />
-        <input {...register("password")} placeholder="password" />
-        <input {...register("password1")} placeholder="password1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("Email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstname", { required: true })}
+          placeholder="firstname"
+        />
+        <input
+          {...register("lastname", { required: true })}
+          placeholder="lastname"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="username"
+        />
+        <input
+          {...register("password", {
+            required: "password is required",
+            minLength: {
+              value: 5,
+              message: "Your password too shorts",
+            },
+          })}
+          placeholder="password"
+        />
+
+        <input
+          {...register("password1", { required: true })}
+          placeholder="password1"
+        />
         <button>add</button>
       </form>
     </div>
